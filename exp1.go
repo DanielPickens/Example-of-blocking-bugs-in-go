9e6b1ce7ac30c078648b54ca06ba20c9ae2229cc
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-03-29 12:35:34 -0600

    Update exp1.go

--- exp1.go
+++ exp1.go
@@ -1,14 +1,14 @@
 func finishReq(timeout time.Duration) r ob {
-2 - ch := make(chan ob)
-3 + ch := make(chan ob, 1)
-4 go func() {
-5 result := fn()
-6 ch <- result // block
-7 } ()
-8 select {
-9 case result = <- ch:
-10 return result
-11 case <- time.After(timeout):
-12 return nil
-13 }
-14 }
+  ch := make(chan ob)
+  ch := make(chan ob, 1)
+ go func() {
+  result := fn()
+  ch <- result // block
+  } ()
+  select {
+ case result = <- ch:
+return result
+case <- time.After(timeout):
+ return nil
+ }
+ }
